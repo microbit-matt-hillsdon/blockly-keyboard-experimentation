6c4c849cb79d5289ebf4e1ad3c669824ca2ee26e
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-02-28 15:53:47 +0000

    fix: comment typos

--- src/shortcut_formatting.ts
+++ src/shortcut_formatting.ts
@@ -1,10 +1,10 @@
 /**
  * Check the platform and return a prefix for the keyboard shortcut.
  * TODO: https://github.com/google/blockly-keyboard-experimentation/issues/155
- * This will eventually be the responsibility of the action code ib
+ * This will eventually be the responsibility of the action code in
  * Blockly core.
  *
- * @param key The key to press in combination with Ctrl of Command.
+ * @param key The key to press in combination with Ctrl or Command.
  * @returns A platform-appropriate string for the meta key.
  */
 export function formatMetaShortcut(key: string) {
