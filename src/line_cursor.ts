d237ef7db4f36b6573157720579ee6c6afcbe8de
Grace <grace@microbit.org>
2025-03-24 13:45:52 +0000

    Revert "fix: blockly menu fails to stay opened when right click"
    This reverts commit 42ef5ed10a3d1ee69a5f502410de45a86130d4ff.

--- src/line_cursor.ts
+++ src/line_cursor.ts
@@ -14,7 +14,7 @@
  */
 
 import * as Blockly from 'blockly/core';
-import {ASTNode, Marker, WidgetDiv} from 'blockly/core';
+import {ASTNode, Marker} from 'blockly/core';
 import {getWorkspaceElement, scrollBoundsIntoView} from './workspace_utilities';
 
 /** Options object for LineCursor instances. */
@@ -539,15 +539,10 @@ export class LineCursor extends Marker {
     // Try to scroll cursor into view.
     if (newNode?.getType() === ASTNode.types.BLOCK) {
       const block = newNode.getLocation() as Blockly.BlockSvg;
-
-      // Repositioning workspace causes blockly menu to hide if we right click
-      // so we don't reposition if it's opened.
-      if (!WidgetDiv.isVisible()) {
-        scrollBoundsIntoView(
-          block.getBoundingRectangleWithoutChildren(),
-          block.workspace,
-        );
-      }
+      scrollBoundsIntoView(
+        block.getBoundingRectangleWithoutChildren(),
+        block.workspace,
+      );
     }
   }
 
